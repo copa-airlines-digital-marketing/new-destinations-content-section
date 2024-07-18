285895662791dde6a586dd8699b41d53490f6ea4
josevch <javch1995@gmail.com>
2024-07-18 17:04:52 -0500

    init for this project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [require('cmds-tailwind-styles')],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
