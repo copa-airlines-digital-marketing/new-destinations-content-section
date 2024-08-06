18fe6c986a21b7a4a50d67eaefd551471a1c0f7a
josevch <javch1995@gmail.com>
2024-08-06 17:36:47 -0500

    providing some little details

--- src/lib/components/DestinationDescription/DestinationsDescription.svelte
+++ src/lib/components/DestinationDescription/DestinationsDescription.svelte
@@ -1,24 +1,50 @@
+<script>import Icon from '@iconify/svelte';</script>
 
-<div class="grid w-auto grid-cols-2 container mx-auto  my-8">
-    <div class=" font-suisse text-grey-600 m-4">
+<div class="grid w-auto xs:grid-cols-1 grid-cols-2 container mx-auto  my-8">
+       <!-- Details Section Start-->
+        <div class="grid grid-rows-[auto_1fr] rounded-2xl border border-solid border-gray-300 ">
+            <div class="bg-primary  p-4 rounded-t-2xl flex gap-2 items-center">
+                <Icon icon="mdi:star-circle"  style="color: #fff" />
+                <h3 class="text-white font-gilroy ">Datos Importantes</h3>
+            </div>
+            
+           
+            <div class=" border border-solid border-gray-300 rounded-b-2xl p-2 grid grid-cols-2 gap-1">
+                <div class="border border-solid border-gray-300 rounded-2xl p-4">   
+                        <Icon icon="mdi:location"  style="color: #0032a0" />
+                        <h4 class="">País</h4>
+                        <p class="">Estados Unidos</p>
+                </div>
+                <div class="border border-solid border-gray-300 rounded-2xl p-4">   
+                        <Icon icon="mdi:location"  style="color: #0032a0" />
+                        <h4 class="">País</h4>
+                        <p class="">Estados Unidos</p>
+                </div>
+                <div class="border border-solid border-gray-300 rounded-2xl p-4">   
+                        <Icon icon="mdi:location"  style="color: #0032a0" />
+                        <h4 class="">País</h4>
+                        <p class="">Estados Unidos</p>
+                </div>
+            </div>
+        </div>
+ 
+            <!-- Details Section End-->
 
-        <p>
-            Prepárate para desvelar un viaje fascinante a través de los museos y vecindarios como Oakwood en Raleigh, donde cada rincón tiene una historia que contar. Explora los distritos históricos y vecindarios animados de Durham, como Ninth Street y Brightleaf Square, en un recorrido fascinante. En cada rincón de Durham, descubrirás una historia rica y cautivadora. Estamos emocionados de que seas parte de la originalidad y vitalidad que define estas dos ciudades.
-    <br>
-            La escena nocturna de Raleigh-Durham está llena de vitalidad y entusiasmo. Desde bares animados con música en vivo hasta clubes nocturnos en el centro de la ciudad, hay alternativas para cada preferencia. ¿Te gustaría experimentar el encanto de la samba en un club cercano o pasar una noche tranquila en un bar con vistas increibles? En Raleigh-Durham, las oportunidades son ilimitadas. 
-            <br>
-           Ahora, déjate llevar por un viaje culinario auténtico. Desde las clásicas barbacoas del sur hasta las innovadoras creaciones, la diversidad culinaria de Raleigh-Durham refleja la riqueza y la autenticidad de la región. Cada bocado cuenta una historia, y estamos aquí para que escribas tu propio capítulo en esta deliciosa travesía.
-            <br>
-           ¡Prepárate para explorar los sabores vibrantes, la diversión y la cultura de Raleigh-Durham! Reserva tus vuelos a Raleigh-Durham y vuela desde el Hub de las Americas® en Panamá con Copa Airlines, la aerolínea más puntual. Descubre el "Triángulo de la Investigación" con experiencias únicas y momentos inolvidables.
-    </div>
-    
-    <div class="grid grid-rows-[auto_1fr] rounded-2xl border border-solid border-gray-300">
-       <div class="bg-primary  p-4 rounded-t-2xl">
-        <h3 class="text-white font-gilroy ">Datos Importantes</h3>
-       </div>
-       <div class="container border border-solid border-gray-300 rounded-b-2xl">
-        <h3>test</h3>
-       </div>
-    </div>
+
+            <!-- Text Content Section Start-->    
+                <div class=" font-suisse text-grey-600 m-4">
+                    <p>
+                        Prepárate para desvelar un viaje fascinante a través de los museos y vecindarios como Oakwood en Raleigh, donde cada rincón tiene una historia que contar. Explora los distritos históricos y vecindarios animados de Durham, como Ninth Street y Brightleaf Square, en un recorrido fascinante. En cada rincón de Durham, descubrirás una historia rica y cautivadora. Estamos emocionados de que seas parte de la originalidad y vitalidad que define estas dos ciudades.
+                        <br>
+                        La escena nocturna de Raleigh-Durham está llena de vitalidad y entusiasmo. Desde bares animados con música en vivo hasta clubes nocturnos en el centro de la ciudad, hay alternativas para cada preferencia. ¿Te gustaría experimentar el encanto de la samba en un club cercano o pasar una noche tranquila en un bar con vistas increibles? En Raleigh-Durham, las oportunidades son ilimitadas. 
+                        <br>
+                    Ahora, déjate llevar por un viaje culinario auténtico. Desde las clásicas barbacoas del sur hasta las innovadoras creaciones, la diversidad culinaria de Raleigh-Durham refleja la riqueza y la autenticidad de la región. Cada bocado cuenta una historia, y estamos aquí para que escribas tu propio capítulo en esta deliciosa travesía.
+                        <br>
+                    ¡Prepárate para explorar los sabores vibrantes, la diversión y la cultura de Raleigh-Durham! Reserva tus vuelos a Raleigh-Durham y vuela desde el Hub de las Americas® en Panamá con Copa Airlines, la aerolínea más puntual. Descubre el "Triángulo de la Investigación" con experiencias únicas y momentos inolvidables.
+                </div>
+            
+            <!-- Text Content Section End-->
+
+ 
 </div>
 
